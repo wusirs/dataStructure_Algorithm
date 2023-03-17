4844d3784dbedf2321c403a3fa30a190e8878d04
wusirs <2411851860@qq.com>
2023-03-17 12:41:12 +0800

    二叉搜索树增加删除节点函数

--- src/e_binary_tree/A_BinarySearchTree.java
+++ src/e_binary_tree/A_BinarySearchTree.java
@@ -73,7 +73,73 @@ public class A_BinarySearchTree<E> implements BinaryTreeInfo {
         size++;
     }
 
-    public E remove() {
+    /**
+     * <1>.删除叶子节点:叶子节点置为null,如果这个二叉树就一个节点,也就是根节点root就是叶子节点,那么直接root=null
+     * <2>.删除度为1的节点:如果要删除的这个节点(a)度为1,那么直接用a的父节点指向a节点的子节点(度为1所以就一个子节点),
+     * 然后让a的子节点的parent指向a的父节点;
+     * 如果这个度为1的节点是根节点,那么直接root指向root的子节点,再让这个子节点的parent为null即可;
+     * <3>.删除度为2的节点:先用前驱或者后继节点覆盖原节点的值,再删除相应的前驱或者后继节点,又因为这个度为2的
+     *
+     * note:如果一个节点的度为2,那么它的前驱和后继节点的度只可能是1或0
+     * @return
+     */
+    public void remove(E element) {
+        node(element);
+    }
+
+    private void remove(Node node){
+        if (node == null) {
+            return;
+        }
+
+        size --;
+        if (node.hasTwoChildren()){ // 度为2的节点
+            // 找后继
+            Node successor = successor(node);
+            // 用后继节点的值覆盖度为2的节点的值
+            node.element = successor.element;
+            // 删除后继节点,只需要把后继节点赋值给node,后面判断删除这个successor节点,这个后继节点的度必然是1或0
+            node = successor;
+        }
+        // 删除node节点,能来到这个位置node的度必然是1或0
+        Node replacement = node.left != null ? node.left : node.right;
+        if (replacement != null){// node是度为1的节点
+            // 更改parent
+            replacement.parent = node.parent;
+            // 更改parent的left或right的指向
+            if (node.parent == null){ // node是度为1的节点并且是根节点
+                this.root = replacement;
+            }else { // node是度为1的节点并且不是根节点
+                if (node == node.parent.left){
+                    node.parent.left = replacement;
+                }else {// node == node.parent.right
+                    node.parent.right = replacement;
+                }
+            }
+        }else if (node.parent == null){// node是叶子节点并且是根节点
+            this.root = null;
+        }else { // node是叶子节点,但不是根节点
+            if (node == node.parent.left){
+                node.parent.left = null;
+            }else { // node == node.parent.right
+                node.parent.right = null;
+            }
+        }
+    }
+
+    private Node<E> node(E element){
+        Node<E> node = root;
+        while (node != null){
+            int cmp = compare(element, node.element);
+            if (cmp == 0) {
+                return node;
+            }
+            if (cmp > 0){
+                node = node.right;
+            }else {
+                node = node.left;
+            }
+        }
         return null;
     }
 
@@ -376,6 +442,60 @@ public class A_BinarySearchTree<E> implements BinaryTreeInfo {
             }
         }
         return height;
+    }
+
+    /**
+     * 前驱节点:中序遍历的前一个节点
+     * @param node
+     * @return
+     */
+    private Node<E> predecessor(Node<E> node){
+        if (node == null){
+            return null;
+        }
+        Node<E> preNode = null;
+
+        // 前驱节点在左子树当中(left.right.right.right.right...)
+        if (node.left != null){
+            preNode = node.left;
+            while (preNode.right != null){
+                preNode = preNode.right;
+            }
+            return preNode;
+        }
+
+        // 一直往上找父节点(a),直到找到父节点(a)是父节点(a)的父节点的左子节点为止
+        while (node.parent != null && node == node.parent.left){
+            node = node.parent;
+        }
+        return  node.parent;
+    }
+
+    /**
+     * 后继节点:中序遍历的后一个节点
+     * @param node
+     * @return
+     */
+    private Node<E> successor(Node<E> node){
+        if (node == null){
+            return null;
+        }
+        Node<E> postNode = null;
+
+        // 前驱节点在左子树当中(right.left.left.left...)
+        if (node.right != null){
+            postNode = node.right;
+            while (postNode.left != null){
+                postNode = postNode.left;
+            }
+            return postNode;
+        }
+
+        // 一直往上找父节点(a),直到找到父节点(a)是父节点(a)的父节点的右子节点为止
+        while (node.parent != null && node == node.parent.right){
+            node = node.parent;
+        }
+        return  node.parent;
     }
 
     private static class Node<E> {
